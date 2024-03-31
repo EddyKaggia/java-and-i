aed8a54d8f9a806138f411c27f0f257cfbabc06b
EddyKaggia <kagzcodes@gmail.com>
2024-03-31 12:24:59 -0700

    feat: extended JComponent

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -1,14 +1,26 @@
+package ch02;
+
+// Abstract Window Toolkit (awt)
+import java.awt.*;
+// Swing is Java's' UI toolkit
 import javax.swing.*;
 
 public class HelloJava {
     public static void main(String[] args) {
         // Create a JFrame object -> Graphical window
         JFrame frame = new JFrame("Hello, Java!");
-        JLabel label = new JLabel("Hello, Java!", JLabel.CENTER);
-        frame.add(label);
+        frame.add(new HelloComponent());
         // Set its size
         frame.setSize(300, 300);
         // Make it visible
         frame.setVisible(true);
     }
 }
+
+class HelloComponent extends JComponent {
+    public void paintComponent(Graphics g) {
+        g.drawString("Hello, Java!", 10, 10);
+    }
+}
+
+
